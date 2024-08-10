b128b84d5e27adfd0239b379ceced21b2612028b
KOTANI\uvmpd <kofma91@gmail.com>
2024-08-10 20:28:43 +0900

    Google Books APIを用いてデータを取得できるよう設定する、APIの処理を実際に実行するapi.rsファイルを追加

---
+++ yomikomi_prj/src/api.rs
@@ -0,0 +1,39 @@
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Debug, Deserialize)]
+pub struct GoogleBooksResponse {
+    pub items: Vec<Volume>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Volume {
+    #[serde(alias = "volumeInfo")]
+    pub volume_info: VolumeInfo,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct VolumeInfo {
+    pub title: String,
+    pub authors: Option<Vec<String>>,
+    pub description: Option<String>,
+    pub image_links: Option<ImageLinks>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ImageLinks {
+    pub thumbnail: Option<String>,
+}
+
+pub fn fetch_books(query: &str) -> Result<GoogleBooksResponse, Box<dyn Error>> {
+    let url = format!(
+        "https://www.googleapis.com/books/v1/volumes?q={}",
+        query
+    );
+
+    let client = Client::new();
+    let response = client.get(&url).send()?.json::<GoogleBooksResponse>()?;
+    
+    Ok(response)
+}
--- yomikomi_prj/src/main.rs
+++ yomikomi_prj/src/main.rs
@@ -1,6 +1,27 @@
-fn main() {
-    let url = "https://www.rust-lang.org";
-    let rc = reqwest::blocking::get(url).unwrap(); //同期処理にするためreqwest::blockingから呼び出す。
-    let contents = rc.text().unwrap();
-    println!("{}", contents);
+mod api;
+use std::error::Error;
+use crate::api::fetch_books;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let query = "夏目漱石";
+    
+    let response = fetch_books(query)?;
+
+    for item in response.items {
+        println!("Title: {}", item.volume_info.title);
+        if let Some(authors) = item.volume_info.authors {
+            println!("Authors: {:?}", authors);
+        }
+        if let Some(description) = item.volume_info.description {
+            println!("Description: {}", description);
+        }
+        if let Some(image_links) = item.volume_info.image_links {
+            if let Some(thumbnail) = image_links.thumbnail {
+                println!("Thumbnail: {}", thumbnail);
+            }
+        }
+        println!("---");
+    }
+
+    Ok(())
 }
