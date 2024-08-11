ac4d360bcf8244d5147301b92ceeda05612ce4b6
KOTANI\uvmpd <kofma91@gmail.com>
2024-08-11 13:32:11 +0900

    fetch_booksメソッドのエラー分岐を行えるようにする

--- yomikomi_prj/src/main.rs
+++ yomikomi_prj/src/main.rs
@@ -2,26 +2,28 @@ mod api;
 use std::error::Error;
 use crate::api::fetch_books;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main(){
     let query = "夏目漱石";
-    
-    let response = fetch_books(query)?;
 
-    for item in response.items {
-        println!("Title: {}", item.volume_info.title);
-        if let Some(authors) = item.volume_info.authors {
-            println!("Authors: {:?}", authors);
-        }
-        if let Some(description) = item.volume_info.description {
-            println!("Description: {}", description);
-        }
-        if let Some(image_links) = item.volume_info.image_links {
-            if let Some(thumbnail) = image_links.thumbnail {
-                println!("Thumbnail: {}", thumbnail);
+    match fetch_books(query) {
+        Ok(response) => {
+            for item in response.items {
+                println!("Title: {}", item.volume_info.title);
+                if let Some(authors) = item.volume_info.authors {
+                    println!("Authors: {:?}", authors);
+                }
+                if let Some(description) = item.volume_info.description {
+                    println!("Description: {}", description);
+                }
+                if let Some(image_links) = item.volume_info.image_links {
+                    if let Some(thumbnail) = image_links.thumbnail {
+                        println!("Thumbnail: {}", thumbnail);
+                    }
+                }
             }
         }
-        println!("---");
+        Err(e) => {
+            println!("エラー:{}",e);
+        }
     }
-
-    Ok(())
 }
