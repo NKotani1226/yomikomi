133f6c8cd259e16ce8800b5a27521b198f435cfe
KOTANI\uvmpd <kofma91@gmail.com>
2024-08-14 16:28:51 +0900

    ページ番号毎にページ送りをする

--- yomikomi_prj/src/api.rs
+++ yomikomi_prj/src/api.rs
@@ -2,10 +2,11 @@ use reqwest::Client;
 use std::error::Error;
 use crate::model::GoogleBooksResponse;
 
-pub async fn fetch_books(query: &str) -> Result<GoogleBooksResponse, Box<dyn Error>> {
+pub async fn fetch_books(query: &str,page_no:u32) -> Result<GoogleBooksResponse, Box<dyn Error>> {
     let url = format!(
-        "https://www.googleapis.com/books/v1/volumes?q={}",
-        query
+        "https://www.googleapis.com/books/v1/volumes?q={}&startIndex={}&orderBy=relevance",
+        query,
+        page_no
     );
 
     let client = Client::new();
--- yomikomi_prj/src/route.rs
+++ yomikomi_prj/src/route.rs
@@ -1,6 +1,6 @@
 use crate::render::render_page;
 use crate::api::fetch_books;
-use actix_web::{get, HttpResponse, Responder,HttpServer, App};
+use actix_web::{get,web, HttpResponse, Responder,HttpServer, App};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -8,7 +8,30 @@ async fn index() -> impl Responder {
     let query = "夏目漱石";
 
     let items = 
-    match fetch_books(query).await {
+    match fetch_books(query,0).await {
+        Ok(response) => {
+            response
+        }
+        Err(_e) => {
+            return HttpResponse::NotFound().finish()
+        }
+    };
+
+    let contents = render_page(items).expect("InternalError");
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(contents)
+}
+
+#[get("/page/{page_no}")]
+async fn page_index(page_no: web::Path<u32>) -> impl Responder {
+    let page_no = page_no.into_inner();
+
+    let query = "夏目漱石";
+
+    let items = 
+    match fetch_books(query,page_no).await {
         Ok(response) => {
             response
         }
@@ -28,6 +51,7 @@ pub async fn create_app(addr: &str, port: u16) -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
+            .service(page_index)
     })
     .bind((addr, port))?
     .run()
