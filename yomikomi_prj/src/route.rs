1b4df366673469b586765d87451b2cfe4983e079
KOTANI\uvmpd <kofma91@gmail.com>
2024-08-22 13:08:56 +0900

    jsonを渡す

--- yomikomi_prj/src/route.rs
+++ yomikomi_prj/src/route.rs
@@ -1,6 +1,5 @@
 use crate::api::fetch_books;
 use actix_web::{web, Responder,HttpServer, App};
-use serde_json::json;
 
 pub async fn create_app(addr: &str, port: u16) -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -19,17 +18,17 @@ async fn index() -> impl Responder {
     let result = fetch_books(query,0).await;
 
     match result {
-        Ok(data) => {
+        Ok(ref data) => {
             // シリアライズして出力
             println!("{}", serde_json::to_string(&data).unwrap());
         }
-        Err(e) => {
+        Err(ref e) => {
             // エラーレスポンスをシリアライズして出力
             println!("{}", serde_json::to_string(&e).unwrap());
         }
     }
 
-    web::Json(json!({ "message": "Hello from Rust backend!" }))
+    web::Json(result)
 }
 
 async fn page_index(page_no: web::Path<u32>) -> impl Responder {
@@ -40,15 +39,15 @@ async fn page_index(page_no: web::Path<u32>) -> impl Responder {
     let result = fetch_books(query,page_no).await;
 
     match result {
-        Ok(data) => {
+        Ok(ref data) => {
             // シリアライズして出力
             println!("{}", serde_json::to_string(&data).unwrap());
         }
-        Err(e) => {
+        Err(ref e) => {
             // エラーレスポンスをシリアライズして出力
             println!("{}", serde_json::to_string(&e).unwrap());
         }
     }
 
-    web::Json(json!({ "message": "Hello from Rust backend!" }))
+    web::Json(result)
 }
